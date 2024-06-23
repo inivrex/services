91da22e81c69d53733341c7be2fe176fd101ea27
inivrex <164545769+inivrex@users.noreply.github.com>
2024-06-23 12:24:42 +0530

    Add files via upload

--- Script/script.js
+++ Script/script.js
@@ -29,20 +29,6 @@ NavMoreLnkBtn.addEventListener("click", () => {
         MoreNavWin.style.height = "0rem";
     }
 })
-function changeTime() {
-    let Fulldate = new Date;
-    let date = Fulldate.getDate();
-    let month = Fulldate.getMonth() + 1;
-    let year = Fulldate.getFullYear();
-    let hours = Fulldate.getHours();
-    let minutes = Fulldate.getMinutes();
-    let seconds = Fulldate.getSeconds();
-    let FinalDate = `${date}-${month}-${year}`;
-    let FinalTime = `${hours}-${minutes}-${seconds}`;
-    dateIn.value = FinalDate;
-    TimeIn.value = FinalTime;
-}
-
 //Remove the id name from url when someone click on nav links
 
 for (let i = 0; i < navlinks.length; i++) {
@@ -51,7 +37,58 @@ for (let i = 0; i < navlinks.length; i++) {
     });
 };
 
-//Adding Date and time when someone submit query.
-setInterval(() => {
-    changeTime();
-}, 1000);
+
+
+// Scroll Navigation Show 
+window.addEventListener('scroll', function () {
+
+    MoreNavMWin.style.display = "none";
+    MoreNavWin.style.height = "0rem";
+    var navbar = document.querySelector('nav');
+    var navbarHeight = navbar.offsetHeight;
+
+    if (window.pageYOffset >= navbarHeight + 200) {
+        navbar.classList.add('fixed');
+        MoreNavWin.style.position = 'fixed';
+        MoreNavWin.style.zIndex = '120';
+        MoreNavWin.style.top = '3.7rem';
+        document.body.style.marginTop = '8rem';
+    } else {
+        navbar.classList.remove('fixed');
+        MoreNavWin.style.position = 'absolute';
+        document.body.style.marginTop = '0rem';
+        document.body.style.transition = '0s';
+    }
+});
+
+
+//Move effect 
+document.addEventListener('DOMContentLoaded', function () {
+    var elements = document.querySelectorAll('.scroll-animate');
+
+    function checkVisibility() {
+        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        elements.forEach(function (element) {
+            var rect = element.getBoundingClientRect();
+            if (rect.top <= (window.innerHeight || document.documentElement.clientHeight) && rect.bottom >= 0) {
+                element.classList.add('visible');
+            }
+        });
+
+        // Reset elements if scroll offset is 0
+        if (scrollTop === 1) {
+            elements.forEach(function (element) {
+                element.classList.remove('visible');
+                // Trigger reflow to restart animation
+                void element.offsetWidth;
+            });
+        }
+    }
+
+    window.addEventListener('scroll', checkVisibility);
+    window.addEventListener('resize', checkVisibility);
+
+    // Initial check
+    checkVisibility();
+});
