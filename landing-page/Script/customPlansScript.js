83790ca950e1e83da663b480a22452b2ccd62ab2
inivrex <164545769+inivrex@users.noreply.github.com>
2024-07-09 19:27:10 +0530

    Add files via upload

--- landing-page/Script/customPlansScript.js
+++ landing-page/Script/customPlansScript.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzbvdl-cqZFs_O5BtBJpLRX2CFvmi_-cq41tzwxxotHnyNjm_d4QhIfYfhqJuIcMqB2nQ/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxYTdp-07MWL-X8HcqhRw0Y_rZjNGU6ncNZTJ7P-kG4ht9nKfkGCUD7KHznPn7Uone2/exec';
 const form = document.forms['google-sheet'];
 const fields = form.querySelectorAll('.required-field');
 const recaptcha = document.querySelector('.g-recaptcha');
